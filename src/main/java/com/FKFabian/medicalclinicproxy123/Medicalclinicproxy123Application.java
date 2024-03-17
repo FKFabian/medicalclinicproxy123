0ebe6af15b4276fb051a55d625a2de9b681fe5d1
FKFabian <fabian.klocek@gmail.com>
2024-03-17 14:07:01 +0100

    first update

---
+++ src/main/java/com/FKFabian/medicalclinicproxy123/Medicalclinicproxy123Application.java
@@ -0,0 +1,13 @@
+package com.FKFabian.medicalclinicproxy123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Medicalclinicproxy123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Medicalclinicproxy123Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/FKFabian/medicalclinicproxy123/Medicalclinicproxy123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FKFabian.medicalclinicproxy123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Medicalclinicproxy123ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
