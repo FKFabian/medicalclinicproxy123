64d6ed9f3df5eacc1fc8715c8f8565090df098c0
FKFabian <fabian.klocek@gmail.com>
2024-03-24 16:11:10 +0100

    Changes by FEFEA

--- src/main/java/com/FKFabian/medicalclinicproxy123/MedicalClinicProxyApplication.java
+++ src/main/java/com/FKFabian/medicalclinicproxy123/MedicalClinicProxyApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Medicalclinicproxy123Application {
+@EnableFeignClients
+public class MedicalClinicProxyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Medicalclinicproxy123Application.class, args);
